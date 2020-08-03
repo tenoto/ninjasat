32548acf40c304c1e2f36b4691a73dfc906dd3b0
Teruaki Enoto <enoto@grpc.ad15.riken.go.jp>
2020-08-03 10:38:48 +0900

    plot

--- tests/plot_ninjasat_orbit.sh
+++ tests/plot_ninjasat_orbit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -f 
 
-plot_satellite_orbit.py -s 2022-09-30_12:02:01 -e 2022-09-30_16:02:01 -o ninjasat_orbit
+plot_satellite_orbit.py -s 2022-09-30_12:02:01 -e 2022-09-30_16:02:01 -o tests/ninjasat_orbit
